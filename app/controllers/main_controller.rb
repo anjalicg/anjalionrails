6c4e9df4bed8a293dfd53b8064d5a4c46ea2c3b8
Anjali C. G <anjalicg2001@gmail.com>
2010-03-18 00:51:07 +0530

    file clean

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,33 +7,13 @@ place=find_location_main(request.remote_addr)
 @visit_details["Your IP"]= request.env["REMOTE_ADDR"]
 @visit_details["Supported Charset"]= request.env["HTTP_ACCEPT_CHARSET"]
 end
-def visit_browser
-@browser = request.http_user_agent
-end
-def visit_place
-@place= "Bangalore"
-end
+
 
 private
-def get_details (str)
-retH=Hash.new
-puts str.class
-=begin
-sscan=StringScanner.new(str)
-sscan.skip_until("HTTP_USER_AGENT")
-ret["browser"]=sscan.scan_until("SERVER_PROTOCOL")
-sscan.skip_until("REMOTE_ADDR")
-ret["ip"]=ssca.scan_until("SERVER_SOFTWARE")
-sscan.skip_until("HTTP_ACCEPT_CHARSET")
-ret["charset"]=sscan.scan_until("REQUEST_METHOD")
-puts ret
-=end
-end
-
 def find_location_main(ip_add)
 require 'timeout'
 require 'net/http'
-ip_add = "122.167.31.3" #this works
+#ip_add = "122.167.31.3" #this works
 begin
 Timeout::timeout(5) {
 	http=Net::HTTP.post_form(URI.parse('http://api.hostip.info/get_html.php'), {'ip'=>ip_add})
