aeab8fba0eb12a777d23d54ac2f894912229aac0
Anjali C. G <anjalicg2001@gmail.com>
2010-03-19 02:50:01 +0530

    main file changed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,19 +1,38 @@
 class MainController < ApplicationController
 def index
+@quotation=Quotation.new
+q=Quotation.find(:all)
+l=rand((q.length) +1)
+if q[l]
+@quote=q[l]
+end
+session["word_hangman"] = "Anjali"
+@word = session["word_hangman"]
 @visit_details=Hash.new()
+unless session["Your Place"]
 place=find_location_main(request.remote_addr)
-@visit_details["Your Place"] = place["city"] +","+ place["country"]
-@visit_details["Your Browser"]= request.env["HTTP_USER_AGENT"]
-@visit_details["Your IP"]= request.env["REMOTE_ADDR"]
-@visit_details["Supported Charset"]= request.env["HTTP_ACCEPT_CHARSET"]
+session["Your Place"] = place["city"] +","+ place["country"]
+session["Your Browser"]= request.env["HTTP_USER_AGENT"]
+session["Your IP"]= request.env["REMOTE_ADDR"]
+session["Supported Charset"]= request.env["HTTP_ACCEPT_CHARSET"]
+end
+@visit_details["Your Place"] = session["Your Place"]
+@visit_details["Your Browser"]= session["Your Browser"]
+@visit_details["Your IP"]= session["Your IP"]
+@visit_details["Supported Charset"]= session["Supported Charset"]
+
+
 end
 
+def clear
+reset_session
+end
 
 private
 def find_location_main(ip_add)
 require 'timeout'
 require 'net/http'
-#ip_add = "122.167.31.3" #this works
+ip_add = "122.167.31.3" #this works
 begin
 Timeout::timeout(5) {
 	http=Net::HTTP.post_form(URI.parse('http://api.hostip.info/get_html.php'), {'ip'=>ip_add})
