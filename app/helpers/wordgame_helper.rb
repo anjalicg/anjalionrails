7da89c48e13e617d65e37738b72830fc2bdb5b49
Anjali C. G <anjalicg2001@gmail.com>
2010-03-21 13:30:25 +0530

    word jumble and vocab

--- app/helpers/wordgame_helper.rb
+++ app/helpers/wordgame_helper.rb
@@ -1,2 +1,16 @@
 module WordgameHelper
+
+def jumbles(words)
+	a_word=words.split(//)
+	len=a_word.length
+	ans=""
+	while len > 0
+		l=rand(len)
+		ans+=a_word[l]
+		a= a_word.delete_at(l)
+		len=a_word.length
+	end #End of while
+	return ans
+end #End of jumble
+
 end
