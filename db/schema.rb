4c0102112ba0cb860cb4a0159da5959fe2cae256
Anjali C. G <anjalicg2001@gmail.com>
2010-04-25 01:30:19 +0530

    db file changed

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100321082803) do
+ActiveRecord::Schema.define(:version => 20100408193203) do
 
   create_table "hangmen", :force => true do |t|
     t.string "word"
