809030175e47ce0a4e605c2a2a8ee0889ef14cdb
Anjali C. G <anjalicg2001@gmail.com>
2010-05-02 02:45:39 +0530

    num days

--- app/helpers/misc_helper.rb
+++ app/helpers/misc_helper.rb
@@ -1,2 +1,16 @@
 module MiscHelper
+def xth(number)
+#This is the function definition
+  case number%10
+    when 1
+    return "st"
+    when 2
+    return "nd"
+    when 3
+    return "rd"
+    else
+    return "th"
+  end
+  
+end
 end
