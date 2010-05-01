dfacad9d5ba1202a9e72043ae08c13fa68a04c75
Anjali C. G <anjalicg2001@gmail.com>
2010-05-01 12:50:50 +0530

    timestamp working

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -4,16 +4,19 @@ case request.method
 when :post
 	case params[:subaction]
 	when "unix2human"
-	puts "Inside timestamp...unix2human"
+	puts "Inside timestamp...unix2human #{params[:misc][:unixT]}"
+	t=Time.new
+	@zone=t.to_s.split(" ")[4]
 	@unix2human = Time.at(params[:misc][:unixT].to_i).strftime("%m-%d-%Y %H:%M:%S")
 	@unixT = params[:misc][:unixT]
+	flash[:notice_unix2human] = "Unix epoch time <b>#{@unixT} = #{@unix2human} GMT #{@zone} </b>"
 	when "human2unix"
 	puts "Inside timestamp...human2unix"
 	ts=params[:misc][:currDnT2]
 	begin
 	date= ts.split(" ")[0].split("-")
 	time = ts.split(" ")[1].split(":")
-	t= Time.local(date[2],date[0],date[1],time[0],time[1],time[2])
+	t= Time.gm(date[2],date[0],date[1],time[0],time[1],time[2])
 	@human2unix = t.to_i
 	@humanT = params[:misc][:currDnT2]
 	rescue
