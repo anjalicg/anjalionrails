5c1d2f729ed47296dbb0d2b379d8d4404f190572
Anjali C. G <anjalicg2001@gmail.com>
2010-05-02 00:30:59 +0530

    unix to human readable and vice versa added

---
+++ app/controllers/misc_controller.rb
@@ -0,0 +1,26 @@
+class MiscController < ApplicationController
+def index
+case request.method
+when :post
+	case params[:subaction]
+	when "unix2human"
+	puts "Inside timestamp...unix2human"
+	@unix2human = Time.at(params[:misc][:unixT].to_i).strftime("%m-%d-%Y %H:%M:%S")
+	@unixT = params[:misc][:unixT]
+	when "human2unix"
+	puts "Inside timestamp...human2unix"
+	ts=params[:misc][:currDnT2]
+	begin
+	date= ts.split(" ")[0].split("-")
+	time = ts.split(" ")[1].split(":")
+	t= Time.local(date[2],date[0],date[1],time[0],time[1],time[2])
+	@human2unix = t.to_i
+	@humanT = params[:misc][:currDnT2]
+	rescue
+	flash[:error_human2unix] = "Please enter time in the format <b>month-date-year Hour(24 hr clock)-minute-second</b>. "
+	end
+	end
+end #case end
+end
+
+end
